b959e694907b704c1b7d8577cf401ef9fe1ad831
reesejmorris <reesejmorris@gmail.com>
2023-03-23 15:04:03 +0000

    Added a link to the first post

--- src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
+++ src/pages/blog/{markdownRemark.frontmatter__slug}.jsx
@@ -1,13 +1,19 @@
 import * as React from "react"
 import { graphql } from "gatsby"
 
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
+
 export default function BlogPostTemplate({
   data, // this prop will be injected by the GraphQL query below.
 }) {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div>
+    <div style={pageStyles}>
       <div>
         <h1>{frontmatter.title}</h1>
         <h2>{frontmatter.date}</h2>
